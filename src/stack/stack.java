6fbd938166140c33ceb500cb03954395edc688cf
Divakar-gaur <divakar.gaur01@gmail.com>
2025-05-01 21:55:16 +0530

    Initial commit

---
+++ src/stack/stack.java
@@ -0,0 +1,35 @@
+package stack;
+
+public class stack {
+    Node head;
+    int count;
+
+    public void push(Object ele) {
+        if (head == null) {
+            head = new Node(ele);
+            count++;
+            return;
+        }
+        head = new Node(ele, head);
+        count++;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count==0;
+    }
+
+    public Object peek(){
+        return head.ele;
+    }
+
+    public Object pop(){
+        Object ele=head.ele;
+        head=head.next;
+        count--;
+        return ele;
+    }
+}
---
+++ src/stack/user.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class user {
+    public static void main(String[] args) {
+        stack s=new stack();
+        System.out.println(s.count);
+    }
+}
